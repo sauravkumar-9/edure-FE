cb70b75f6c3885e02c28fc912310b4482cc50f6c
Saurav Kumar <sauravkumar@192.168.1.6>
2025-06-26 21:31:13 +0530

    Git gub hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ed-tech/',
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: [
