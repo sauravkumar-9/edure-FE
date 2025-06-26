169551243e87f19006290a3b0355c0e574ae34af
Saurav Kumar <sauravkumar@192.168.1.6>
2025-06-26 21:50:40 +0530

    Git gub hosting

--- src/router.tsx
+++ src/router.tsx
@@ -22,107 +22,112 @@ import { CompaniesList } from "./pages/placement/companiesList";
 import { PlacementCatchupDashboard } from "./pages/placement/catchup";
 import ExamsPage from "./pages/exam/examDetails";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigation />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to="/student/catchup" replace />,
+        },
+        {
+          path: "management",
+          children: [
+            {
+              index: true,
+              element: <Navigate to="catchup" replace />,
+            },
+            { path: "catchup", element: <ManagementDashboard /> },
+          ],
+        },
+        {
+          path: "report",
+          children: [
+            {
+              index: true,
+              element: <Navigate to="catchup" replace />,
+            },
+            { path: "list", element: <ReportListing /> },
+            { path: "list/:id", element: <Report /> },
+          ],
+        },
+        {
+          path: "teacher",
+          children: [
+            {
+              index: true,
+              element: <Navigate to="catchup" replace />,
+            },
+            { path: "catchup", element: <TeacherDashboard /> },
+          ],
+        },
+        {
+          path: "student",
+          children: [
+            {
+              index: true,
+              element: <Navigate to="catchup" replace />,
+            },
+            { path: "profile", element: <StudentProfile /> },
+            { path: "catchup", element: <StudentCatchUp /> },
+            { path: "attendence", element: <StudentAttendence /> },
+            { path: "grade", element: <StudentGrade /> },
+            { path: "assignments", element: <Assignments /> },
+          ],
+        },
+        {
+          path: "placement",
+          children: [
+            {
+              index: true,
+              element: <Navigate to="catchup" replace />,
+            },
+            { path: "catchup", element: <PlacementCatchupDashboard /> },
+            { path: "list", element: <CurrentCoursesTab /> },
+            { path: "list/:id", element: <PlacementDetails /> },
+            { path: "dashboard", element: <PlacementDashboard /> },
+            { path: "companyList", element: <CompaniesList /> },
+          ],
+        },
+        {
+          path: "exam",
+          children: [
+            {
+              index: true,
+              element: <Navigate to="details" replace />,
+            },
+            { path: "details", element: <ExamsPage /> },
+          ],
+        },
+        {
+          path: "admission",
+          element: <Admission />,
+          children: [
+            {
+              index: true,
+              element: <Navigate to="insights" replace />,
+            },
+            {
+              path: "insights",
+              element: <AdmissionInsights />,
+            },
+            {
+              path: "report",
+              element: <AdmissionReport />,
+            },
+            // {
+            //   path: "*",
+            //   element: <Navigate to="insights" replace />,
+            // },
+          ],
+        },
+      ],
+    },
+    { path: "/admission/form", element: <AdmissionForm /> },
+  ],
   {
-    path: "/",
-    element: <Navigation />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/student/catchup" replace />,
-      },
-      {
-        path: "management",
-        children: [
-          {
-            index: true,
-            element: <Navigate to="catchup" replace />,
-          },
-          { path: "catchup", element: <ManagementDashboard /> },
-        ],
-      },
-      {
-        path: "report",
-        children: [
-          {
-            index: true,
-            element: <Navigate to="catchup" replace />,
-          },
-          { path: "list", element: <ReportListing /> },
-          { path: "list/:id", element: <Report /> },
-        ],
-      },
-      {
-        path: "teacher",
-        children: [
-          {
-            index: true,
-            element: <Navigate to="catchup" replace />,
-          },
-          { path: "catchup", element: <TeacherDashboard /> },
-        ],
-      },
-      {
-        path: "student",
-        children: [
-          {
-            index: true,
-            element: <Navigate to="catchup" replace />,
-          },
-          { path: "profile", element: <StudentProfile /> },
-          { path: "catchup", element: <StudentCatchUp /> },
-          { path: "attendence", element: <StudentAttendence /> },
-          { path: "grade", element: <StudentGrade /> },
-          { path: "assignments", element: <Assignments /> },
-        ],
-      },
-      {
-        path: "placement",
-        children: [
-          {
-            index: true,
-            element: <Navigate to="catchup" replace />,
-          },
-          { path: "catchup", element: <PlacementCatchupDashboard /> },
-          { path: "list", element: <CurrentCoursesTab /> },
-          { path: "list/:id", element: <PlacementDetails /> },
-          { path: "dashboard", element: <PlacementDashboard /> },
-          { path: "companyList", element: <CompaniesList /> },
-        ],
-      },
-      {
-        path: "exam",
-        children: [
-          {
-            index: true,
-            element: <Navigate to="details" replace />,
-          },
-          { path: "details", element: <ExamsPage /> },
-        ],
-      },
-      {
-        path: "admission",
-        element: <Admission />,
-        children: [
-          {
-            index: true,
-            element: <Navigate to="insights" replace />,
-          },
-          {
-            path: "insights",
-            element: <AdmissionInsights />,
-          },
-          {
-            path: "report",
-            element: <AdmissionReport />,
-          },
-          // {
-          //   path: "*",
-          //   element: <Navigate to="insights" replace />,
-          // },
-        ],
-      },
-    ],
-  },
-  { path: "/admission/form", element: <AdmissionForm /> },
-]);
+    basename: "/ed-tech/", // Add this for GitHub Pages
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/ed-tech/',
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: [
