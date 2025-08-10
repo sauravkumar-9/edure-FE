889d7b94409d2fa08bcb118bf736801530ca8caa
Saurav Kumar <sauravkumar@192.168.1.9>
2025-08-10 14:44:14 +0530

    Comp structure

--- src/components/dialog/dialogDataAction.tsx
+++ src/components/dialog/dialogDataAction.tsx
@@ -9,6 +9,14 @@ import {
 import DashboardTabs from "../other/tabs";
 import { ConfigActionFooter } from "../final/configActionFoooter";
 
+const dialogSizeMap: any = {
+  LARGE: { height: 700, width: 900 },
+  MEDIUM: { height: 500, width: 700 },
+  SMALL: { height: 400, width: 500 },
+  ALERT: { height: 300, width: 400 },
+  DEFAULT: { height: 700, width: 900 },
+};
+
 export default function DialogDataAction(props: any) {
   const {
     isDialogOpen,
@@ -21,18 +29,23 @@ export default function DialogDataAction(props: any) {
     handleDiscard,
     isDraft,
     isSubmissionAllowed,
+    dialogType = "DEFAULT",
   } = props;
 
   const hasSingleTab = tabsDetails.length === 1;
   const SingleComponent = hasSingleTab ? tabsDetails[0].component : null;
   const singleTabProps = hasSingleTab ? tabsDetails[0].props : null;
 
+  const dialogSize = dialogSizeMap[dialogType];
+
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger asChild>
         <Button variant="default">{actionButtonLabel}</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[900px] h-[700px] flex flex-col">
+      <DialogContent
+        className={`sm:max-w-[${dialogSize.width}px] h-[${dialogSize.height}px] flex flex-col`}
+      >
         <DialogHeader className="pb-2">
           <DialogTitle>{dialogTitle}</DialogTitle>
         </DialogHeader>
